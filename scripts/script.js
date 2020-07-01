254bd22ba1d738cc606ebf0ef733a7efe9814e53
imigammidge <64850728+imigammidge@users.noreply.github.com>
2020-07-01 20:48:50 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -17,31 +17,26 @@ function topFunction() {
 }
 // ---- Scroll back to top button END ---- //
 
-
 // Subscribe button modal pop up //
-// Get the modal
-var modal = document.getElementById("myModal")
-// Get the button that opens the modal
-var btn = document.getElementById("sub-button");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var modal = document.querySelector(".modal");
+var trigger = document.getElementById("sub-button");
+var closeButton = document.querySelector(".close");
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function toggleModal() {
+   modal.classList.toggle("show-modal");
 }
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+
+function windowOnClick(event) {
+   if (event.target === modal) {
+            toggleModal();
+   }
 }
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-// Subscribe button modal pop up END //
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
 
 // END
   
